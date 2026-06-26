626f447cae64eb8a9529c765fa795c99c9ee57ea
Archi <shaharchi868@gmail.com>
2026-06-26 15:01:42 +0530

    signup completed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,24 @@
-exports.signup = (req,res)=>{
-    console.log(req.body);
+const bcrypt = require("bcryptjs");
+const User=require("../models/user");
+exports.signup =async  (req,res)=>{
+    const {name,email,password}=req.body;
+      const existingUser = await User.findOne({ email });
 
-    res.json({
-        message:"Data received"
+    if(existingUser){
+        return res.status(400).json({
+            message:"User already exists"
+        });
+    }
+    const hashedPassword = await bcrypt.hash(password,10);
+     const user = new User({
+        name,
+        email,
+        password: hashedPassword
+    });
+
+    await user.save();
+
+    res.status(201).json({
+        message:"User created successfully"
     });
 };
